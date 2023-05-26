79ee9db959e29d959821c8755e0ec68b2fc2296b
touthecode <130022845+touthecode@users.noreply.github.com>
2023-05-26 22:34:20 +0800

    added redo ability to the addition function

--- pycalculator.py
+++ pycalculator.py
@@ -13,3 +13,12 @@ while True:
             print("This is your output: ", sum)
         except ValueError:
             print("An error has been detected. Proceeding to redo function.")
+
+        # Create an option to repeat the program
+        print("Do you still wish to use this program: y/n?")
+        choice = input(": ").lower
+        if choice == "y":
+            True
+        elif choice == "n":
+            break
+   
