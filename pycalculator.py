aa1f42094f872f682d197a047eb7da90cc8608a3
touthecode <130022845+touthecode@users.noreply.github.com>
2023-05-26 22:51:49 +0800

    add if and elif functions for subtraction

--- pycalculator.py
+++ pycalculator.py
@@ -3,13 +3,13 @@ while True:
     print("Please press: 1 for Addition, 2 for Subtraction, 3 for Multiplication, 4 for Division")
     decision = input("Enter the number of your choice: ")
 
-    # Create a function for ADDITION
+    # Create an if and elif function for ADDITION
     if decision == "1":
         print("You have selected Addition mode! Prepare to input two numbers to be combined.")
         try:
             num1 = input("Enter your 1st number: ")
             num2 = input("Enter your 2nd number: ")
-            sum = int(num1) + int(num2)
+            sum = float(num1) + float(num2)
             print("This is your output: ", sum)
         except ValueError:
             print("An error has been detected. Proceeding to redo function.")
@@ -21,4 +21,15 @@ while True:
             True
         elif choice == "n":
             break
+    
+     # Create an if and elif function for ADDITION
+    if decision == "2":
+        print("You have selected Subtraction mode! Prepare to input two numbers to be included.")
+        try:
+            num1 = input("Enter your 1st number: ")
+            num2 = input("Enter your 2nd number: ")
+            difference = float(num1) - float(num2)
+            print("This is your output: ", difference)
+        except ValueError:
+            print("An error has been detected. Proceeding to redo function.")
    
