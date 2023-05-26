e77d5543b2ddc3b2a6441deeaf1bc548640089e7
touthecode <130022845+touthecode@users.noreply.github.com>
2023-05-26 22:14:15 +0800

    started with while loop

---
+++ pycalculator.py
@@ -0,0 +1,4 @@
+# Create a while function to enable repeatability in the program
+while True:
+    print("Please press: 1 for Addition, 2 for Subtraction, 3 for Multiplication, 4 for Division")
+    decision = input("Enter the number of your choice: ")
